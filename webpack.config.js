63e9877c9ac3debdcc6c66f998672d0eff37fbfb
juntao.chen <153299521@qq.com>
2023-06-21 10:47:52 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve, join } = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: resolve(__dirname, 'src/index.ts'),
+  output: {
+    filename: 'index.js',
+    clean: true
+  },
+  devServer: {
+    port: 8888,
+    static: {
+      directory: join(__dirname, 'public')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          // 使用babel.config.js
+          options: {
+            presets: [
+              [
+                '@babel/preset-env' // 将ES6语法转成ES5
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: resolve(__dirname, './tsconfig.json')
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-ts',
+      template: './public/index.html'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      // chunks: 'all'
+      minSize: 1000 * 1024
+    }
+  }
+}
